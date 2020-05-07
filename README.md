d0b65ad979e7ae959edd0fbcf90e9005de3194ce
hassiweb <nryk.hashimoto@gmail.com>
2020-05-08 00:14:17 +0900

    Finalize

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 こちらの記事をご覧ください。
 
-| hassiweb's programming
+[ネットワーク越しの RTL-SDR で SDR# を使う方法 | hassiweb's programming](https://hassiweb-programming.blogspot.com/2020/05/sdrsharp-using-remote-rtl-sdr.html)
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -it --rm --device=/dev/bus/usb:/dev/bus/usb -p 1234:1234 rtl-sdr-server
+docker run -it --rm --device=/dev/bus/usb:/dev/bus/usb -p 1234:1234 registry.gitlab.com/hassiweb-programming/docker-rtl-sdr/rtl-sdr:1.0
